a4ecd584a7ab720d4f7a83e352b216e16c436953
Christopher Ozurumba <jaycees20@yahoo.com>
2025-03-21 16:45:22 +0000

    update code

--- etl.py
+++ etl.py
@@ -15,28 +15,25 @@ invoice_df.show(10)
 
 # Replace empty values in column billing_state with not applicable
 transform_df = invoice_df.withColumn("billing_state", when(col("billing_state") == "", "N/A").otherwise(col("billing_state")))
+
+# Display the value
 transform_df.show(10)
-
-
-
-
-# # For uniformity of records in billing_postal_code, change datatype to string
-# transform_df = transform_df.withColumn("billing_postal_code", col("billing_postal_code").cast("string"))
-# transform_df.printSchema()
-# invoice_df.printSchema()
 
 # Load transformed data to destination 
 transform_df.write.csv('Data/transformed/transformed_invoice.csv', header=True, mode='overwrite')
 
 # Aggregation of sales by day
 aggregate_day_df = transform_df.groupBy('invoice_date').agg(_sum('total').alias('daily_sales'))
+
+# Display the value
 aggregate_day_df.show(40)
 
 # Aggregation of sales by week
 aggregate_week_df = transform_df.groupBy(year(col("invoice_date")).alias("year"), 
                                     weekofyear(col("invoice_date")).alias("week_number")) \
-                           .agg(_sum("total").alias("weekly_sales")) \
-                           .orderBy("year", "week_number")
+                                .agg(_sum("total").alias("weekly_sales")) \
+                                .orderBy("year", "week_number")
+
 
 # Display the value
 aggregate_week_df.show()
